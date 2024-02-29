41641d929ca3e3f20589445b9272db3cdeba3075
theoliberkeys <160034208+theoliberkeys@users.noreply.github.com>
2024-02-29 13:09:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 function format_agent(nom) {
-        var lien_agent = nom.toLowerCase();
-        lien_agent = lien_agent.replace(/ /g, '-');
-        lien_agent = lien_agent.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        return lien_agent;
+    var lien_agent = nom.toLowerCase();
+    lien_agent = lien_agent.replace(/ /g, '-');
+    lien_agent = lien_agent.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return lien_agent;
 }
 
 // Récupère le numéro de téléphone
 function get_phone() {
-        var numero = document.getElementById("numero").value;
-        numero = numero.replace(/\s/g, '');
-        numero = numero.replace(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, '$1 $2 $3 $4 $5');
-        return numero;
+    var numero = document.getElementById("numero").value;
+    numero = numero.replace(/\s/g, '');
+    numero = numero.replace(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, '$1 $2 $3 $4 $5');
+    return numero;
 }
 
 // Récupère la photo
 function get_photo() {
-        var imageInput = document.getElementById('imageInput');
-        var image1 = new Image();
-        console.log(imageInput.files[0])
-        console.log(imageInput)
-        image1.src = URL.createObjectURL(imageInput.files[0]);
-        return image1.src;
+    var imageInput = document.getElementById('imageInput');
+    var image1 = new Image();
+    console.log(imageInput.files[0])
+    console.log(imageInput)
+    image1.src = URL.createObjectURL(imageInput.files[0]);
+    return image1.src;
 }
 
 // Génère le QRCode
 function get_QRCode(lien_agent) {
-        const CodeQR = new QRCodeStyling({
-            width: 200,
-            height: 200,
-            type: "png",
-            data: `https://liberkeys.com/agents/${lien_agent}`,
-            image: "https://uploads-ssl.webflow.com/6022985415e9f66f2904c07b/65ccaff32f54e9dbba3a4dd4_lk.png",
-            dotsOptions: {
-                color: "#241C1A",
-                type: ""
-            },
-            backgroundOptions: {
-                color: "#ffffff",
-            },
-            imageOptions: {
-                crossOrigin: "anonymous",
-                margin: -60
-            }
-        });
-        return CodeQR;
+    const CodeQR = new QRCodeStyling({
+        width: 200,
+        height: 200,
+        type: "png",
+        data: `https://liberkeys.com/agents/${lien_agent}`,
+        image: "https://uploads-ssl.webflow.com/6022985415e9f66f2904c07b/65ccaff32f54e9dbba3a4dd4_lk.png",
+        dotsOptions: {
+            color: "#241C1A",
+            type: ""
+        },
+        backgroundOptions: {
+            color: "#ffffff",
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: -60
+        }
+    });
+    return CodeQR;
 }
 
 // Récupère le métier
 function get_metier() {
-        var metier = document.querySelector('input[name="metier"]:checked').value;
-        return metier;
+    var metier = document.querySelector('input[name="metier"]:checked').value;
+    return metier;
 }
 
 // Dessine une image
 function draw_Image(source, page, axeX, axeY, hauteur, largeur) {
-        page.drawImage(source, {
-            x: axeX,
-            y: axeY,
-            width: largeur,
-            height: hauteur
-        })
+    page.drawImage(source, {
+        x: axeX,
+        y: axeY,
+        width: largeur,
+        height: hauteur
+    })
 }
 
 // Dessine du texte
 function draw_Text(source, page, axeX, axeY, taille, police, couleur) {
-        page.drawText(source, {
-            x: axeX,
-            y: axeY,
-            size: taille,
-            font: police,
-            color: couleur
-        })
+    page.drawText(source, {
+        x: axeX,
+        y: axeY,
+        size: taille,
+        font: police,
+        color: couleur
+    })
 }
 
 // Récupère les bibliothèques nécessaires
@@ -130,8 +130,7 @@ async function panneau_simple(nom, numero, fichier, photo) {
     const pdfBytes = await pdfDoc.save()
 
     // Enregistre le fichier
-    var filename = fichier.includes("vendu") ? 'VENDU' : 'AV';
-    download(pdfBytes, `PANNEAU_SIMPLE_${filename}.pdf`, "application/pdf");
+    return pdfBytes;
 }
 
 
@@ -181,8 +180,7 @@ async function panneau_double(nom, numero, fichier, photo) {
     const pdfBytes = await pdfDoc.save()
 
     // Enregistre le fichier
-    var filename = fichier.includes("vendu") ? 'VENDU' : 'AV';
-    download(pdfBytes, `PANNEAU_DOUBLE_${filename}.pdf`, "application/pdf");
+    return pdfBytes;
 }
 
 
@@ -241,7 +239,7 @@ async function carte_de_visite(nom, mail, metier, numero, fichier, photo) {
     const pdfBytes = await pdfDoc.save()
 
     // Enregistre le fichier
-    download(pdfBytes, 'CDV.pdf', "application/pdf");
+    return pdfBytes;
 }
 
 // Fonction pour réaliser le flyer
@@ -303,5 +301,5 @@ async function flyer(nom, mail, metier, numero, fichier, photo) {
     const pdfBytes = await pdfDoc.save()
 
     // Enregistre le fichier
-    download(pdfBytes, "FLYER.pdf", "application/pdf");
+    return pdfBytes;
 }
