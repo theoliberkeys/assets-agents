1fccded0ff937895b485cb5ef199c8f7313dcfc9
theoliberkeys <160034208+theoliberkeys@users.noreply.github.com>
2025-01-16 13:52:44 +0100

    Update and rename new2.js to new3.js

--- new3.js
+++ new3.js
@@ -221,7 +221,7 @@ async function panneau_simple(nom, numero, fichier, photo) {
 
     const rsacText = get_RSAC();
     console.log(rsacText);
-    draw_Text(rsacText, firstPage, 15, 256, 9, ppregu, beige, 90);
+    draw_Text(rsacText, firstPage, 30, 770, 16, ppregu, beige, 90);
 
     const pdfBytes = await pdfDoc.save()
 
